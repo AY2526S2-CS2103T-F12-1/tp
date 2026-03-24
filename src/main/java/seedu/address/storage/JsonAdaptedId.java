bc95a76ff43b74b11c155819cc00b96d2984edac
Tristanlimyr <87318834+Tristanlimyr@users.noreply.github.com>
2026-03-24 12:23:47 +0800

    Edit code to comply with coding standard

--- src/main/java/seedu/address/storage/JsonAdaptedId.java
+++ src/main/java/seedu/address/storage/JsonAdaptedId.java
@@ -2,9 +2,9 @@ package seedu.address.storage;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.id.Id;
-import seedu.address.model.itinerary.Itinerary;
 
 /**
  * Jackson-friendly version of {@link Id}
